376615cf3d980c51fd6565be410b60d1f5e50c5f
Boisier <valentin@dufois.fr>
2018-03-25 20:08:11 +0200

    Adding an API library for REST requests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // React
 import React, { Component } from 'react'
+
 // Scene dependencies
 import './App.css'
+
 // Scene componentts
 import Header from './scenes/Header/Header'
+import REST from './scenes/RESTTest'
 
 export default class extends Component {
   render = () => (
     <section className="App">
       <Header/>
+      <REST/>
     </section>
   )
 }
