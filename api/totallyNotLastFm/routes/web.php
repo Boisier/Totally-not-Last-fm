3ee1b50e7ef6e015bc003867ee3eeb9fd2d1195b
audreycombe <acombe@etud.u-pem.fr>
2018-05-06 14:56:53 +0200

    create routes for the albums

--- api/totallyNotLastFm/routes/web.php
+++ api/totallyNotLastFm/routes/web.php
@@ -11,11 +11,31 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$app->get('/', function () use ($app) {
+    return $app->version();
 });
 
-
+/*
 $router->get('/user/{name}', function ($name) {
     return $name;
 });
+*/
+
+//-----------------Albums------------------
+/* GetAllAlbums
+ *return all the albums
+ */
+$app->get('/albums', 'AlbumController@getAllAlbums'); 
+$app->post('/albums', 'AlbumController@createAlbum'); //create an album
+$app->get('/albums/{album_id_album}', 'AlbumController@getAlbum'); //return a specific album
+$app->put('/albums/{album_id_album}', 'AlbumController@updateAlbum'); //modify an album
+$app->delete('/albums/{album_id_album}', 'AlbumController@deleteAlbum'); //delete an album
+
+
+
+/* Add an album 
+ Datas : 
+ 'album_title_album' => 'string'
+ 'album_nb_tracks' => 'integer'
+ */
+//$router->post('/albums', 'AlbumController@')
