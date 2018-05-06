c7dc7df7cf02aac225738dfd793d428960f8f402
Guillaume DUT <GuillaumeDUT@github.com>
2018-05-06 11:55:28 +0200

    Adding seeder files to create fake data

--- api/totallyNotLastFm/database/factories/ModelFactory.php
+++ api/totallyNotLastFm/database/factories/ModelFactory.php
@@ -11,9 +11,87 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-    ];
+$factory->define(App\Album::class, function (Faker\Generator $faker) {
+  return [
+    'album_id_album' => $faker->id,
+    'album_title_album' => $faker->title,
+    'album_nb_tracks' => $faker->mt_rand(5,15)
+  ];
 });
+
+$factory->define(App\Artist::class, function(Faker\Generator $faker){
+  return [
+    'artist_id' => $faker->id,
+    'artist_name' => $faker->name,
+    'artist_birth_year' => $faker->date("Y-m-d",mt_rand(1262055681,1262055681)),
+    'artist_death_year'=> 'NULL'
+  ];
+});
+
+$factory->define(App\Genre::class, function(Faker\Generator $faker){
+  return [
+    'genre_id_genre' => $faker->id,
+    'genre_name_genre' => $faker->name
+  ];
+});
+
+$factory->define(App\History::class, function(Faker\Generator $faker){
+  return [
+    'history_id_history' => $faker->id,
+    'user_id_user' => $faker->id,
+    'history_play_time' => $faker->,
+  ];
+});
+
+$factory->define(App\Music::class, function(Faker\Generator $faker){
+  return [
+    'music_id_music' => $faker->id,
+    'music_title' => $faker->title,
+    'music_duration' => '04:50',
+    'music_release_date' => $faker->date("Y-m-d"),
+  ];
+});
+
+$factory->define(App\Nationality::class, function(Faker\Generator $faker){
+  return [
+    'nationality_id_nationality' => $faker->id,
+    'nationality_code' => 'FR',
+  ];
+});
+
+$factory->define(App\Playlist::class, function(Faker\Generator $faker){
+  return [
+    'playlist_id_playlist' => $faker->id,
+});
+
+$factory->define(App\Spotify::class, function(Faker\Generator $faker){
+  return [
+    'spotify_user_id' => $faker->id,
+    'user_id_user' => $faker->id
+  ];
+});
+
+$factory->define(App\Spotify::class, function(Faker\Generator $faker){
+  return [
+    'user_id_user' => $faker->id,
+  ];
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- api/totallyNotLastFm/database/seeds/DatabaseSeeder.php
+++ api/totallyNotLastFm/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Album;
+use App\Artist;
+use App\Genre;
+use App\History;
+use App\Music;
+use App\Nationality;
+use App\Playlist;
+use App\Spotify;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +21,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
+      DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+      
+        
+      factory(Album::class,100)->create();
+      factory(Artist::class,100)->create();
+      factory(Genre::class,100)->create();
+      factory(History::class,100)->create();
+      factory(Music::class,100)->create();
+      factory(Nationality::class,100)->create();
+      factory(Playlist::class,100)->create();
+      factory(Spotify::class,100)->create();
+      factory(User::class,100)->create();
+      
+      DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
