3f316f7af1ae651d6b06dc056e782bf31f4cc217
Val <valentin@dufois.fr>
2018-05-10 20:13:57 +0200

    Finishing up Setting page content

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
 import StatsPeriodesNavBar from './scenes/StatsPeriodesNavBar/StatsPeriodesNavBar'
 
@@ -7,7 +6,6 @@ export default class extends Component {
   render = () => (
     <section className="home-page">
       <StatsPeriodesNavBar />
-      <Link to="/about">ABOUT</Link>
     </section>
   )
 }
--- src/scenes/Settings/Settings.js
+++ src/scenes/Settings/Settings.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
 import EditableFieldInput from 'containers/EditableFieldInput/EditableFieldInput'
+import LinkToSpotify from './scenes/LinkToSpotify/LinkToSpotify'
+import LinkedSpotify from './scenes/LinkedSpotify/LinkedSpotify'
 
 export default class extends Component {
   constructor (props) {
@@ -8,12 +10,12 @@ export default class extends Component {
     /*
     Retrieve user infos from server
      */
-    this.setState({
+    this.state = {
       user: {
-        email: "test@test.fr",
-        spotifyLink: false
+        email: 'test@test.fr',
+        spotify: null
       }
-    })
+    }
   }
   updateEmail = (newEmail) => {
     console.log(newEmail)
@@ -29,21 +31,36 @@ export default class extends Component {
     return (
       <section className="settings-page">
         <h2>Profile</h2>
-        <h3>E-mail</h3>
-        <EditableFieldInput
-          type="text"
-          label="Email address"
-          value={this.state.user.email}
-          onConfirm={this.updateEmail} />
-        <h3>Password</h3>
-        <EditableFieldInput
-          type="password"
-          label="New password"
-          value={''}
-          confirm
-          onConfirm={this.updatePassword} />
+        <div className="settings-section">
+          <h3>E-mail</h3>
+          <EditableFieldInput
+            type="text"
+            label="Email address"
+            value={this.state.user.email}
+            onConfirm={this.updateEmail} />
+          <h3>Password</h3>
+          <EditableFieldInput
+            type="password"
+            label="New password"
+            value={''}
+            confirm
+            onConfirm={this.updatePassword} />
+          <p className="settings-disclaimer">
+            Your profile information are used to log on <i>Totally not Last.fm</i>.<br />
+            We do not share these information with anyone.
+          </p>
+        </div>
         <h2>Spotify</h2>
+        { this.state.user.spotify ? <LinkedSpotify /> : <LinkToSpotify /> }
         <h2 className="text-danger">Danger zone</h2>
+        <div className="settings-section">
+          <div className="form-btn remove-account-btn">Remove my account</div>
+          <p className="settings-disclaimer">
+            This will erase all your data from our database.<br />
+            If you want to disconnect your spotify account, please use the button above.<br />
+            Removing your account cannot be undone. You'll need to register again, and you will lost your stats history.
+          </p>
+        </div>
       </section>
     )
   }
