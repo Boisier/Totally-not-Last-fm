936a06c5da2279ec4c33319c66e0bfe168a6bdb7
Val <valentin@dufois.fr>
2018-05-13 18:28:47 +0200

    Animation when switching section

--- src/scenes/Home/scenes/StatsSection/StatsSection.js
+++ src/scenes/Home/scenes/StatsSection/StatsSection.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 import stats from 'library/stats'
 
@@ -12,27 +13,47 @@ export default class extends Component {
     this.state = {
       period: this.props.period,
       periodName: stats.periods[this.props.period].name,
-      currentSection: 0
+      currentSection: 0,
+      oldSection: 0
     }
   }
 
   onSectionChange = (newSection) => {
     console.log(this.state.periodName + ' ' + newSection)
     this.setState({
-      currentSection: newSection
+      currentSection: newSection,
+      oldSection: this.state.currentSection
     })
   }
 
   render () {
+    const statBlocks = (
+      <div className="stats-wrapper" key={this.state.periodName + '-' + this.state.currentSection}>
+        <StatBlock size="wide" />
+        <StatBlock />
+        <StatBlock />
+      </div>
+    )
+
     return (
       <div className="stats-section">
         <PeriodSections
           period={this.props.period}
           section={this.state.currentSection}
           onSectionChange={this.onSectionChange} />
-        <StatBlock size="wide" />
-        <StatBlock />
-        <StatBlock />
+        <ReactCSSTransitionGroup
+          transitionName={
+            this.state.currentSection > this.state.oldSection
+              ? 'stats-wrapper-left' : 'stats-wrapper-right'
+          }
+          transitionEnterTimeout={150}
+          transitionLeaveTimeout={150}
+          transitionEnter={true}
+          transitionLeave={true}
+          component="div"
+          className="stats-wrapper-animation">
+          {statBlocks}
+        </ReactCSSTransitionGroup>
       </div>
     )
   }
