9b6f6fbcf6ef565328411c499ff7cc02edfd4b40
Val <valentin@dufois.fr>
2018-05-17 18:26:04 +0200

    Doughnut graphs updates There is still a bug with the tooltip. The data seems to be handled like a bar charts :/

--- src/containers/Graph/DoughnutGraph.js
+++ src/containers/Graph/DoughnutGraph.js
@@ -1,34 +1,60 @@
 import React from 'react'
-import GraphEntity from './GraphEntity'
+import GraphEntity from 'Containers/Graph/GraphEntity'
+import _ from 'lodash'
 
 import {Doughnut} from 'react-chartjs-2'
 import doughnutStyle from './styles/doughnut.json'
 import { interpolateRgb } from 'd3-interpolate'
+import { doughnutTooltip } from './tooltips'
 
 export default class extends GraphEntity {
-  genData () {
-    const getColor = interpolateRgb()
+  genData = () => {
+    const getColor = interpolateRgb(this.props.toColor, this.props.fromColor)
     const gradientStops = this.props.data.map((val, index, { length }) => {
       return getColor(index / length)
     })
+
+    /* const data = this.props.data.map((val, i) => {
+      return {
+        value: val,
+        label: this.props.labels[i] ? this.props.labels[i] : ''
+      }
+    })
+
+    console.log(data) */
+
     // Create gradient
-    this.setState({
+    this.setState({ graphData: {
       labels: this.props.labels,
       datasets: [{
-        data: this.props.data,
+        type: 'doughnut',
+        data: _.take(this.props.data, this.props.labels.length),
         backgroundColor: gradientStops,
         pointBackgroundColor: 'transparent',
-        borderColor: 'transparent'
+        borderColor: '#FFF',
+        borderWidth: 2,
+        hoverBorderColor: '#FFF',
+        hoverBorderWidth: 2
       }]
-    })
+    }})
+
+    // document.getElementById('tooltip-' + this.props.graphID).style.backgroundColor = this.props.toColor
   }
 
   getGraphOption () {
     let graphOptions = doughnutStyle
+    graphOptions.tooltips.custom = doughnutTooltip
+
     return graphOptions
   }
 
   render = () => {
-    return super.render(Doughnut)
+    return <Doughnut
+      data={[1, 8, 3, 7, 12]}
+      width={100}
+      height={50}
+      options={{
+        maintainAspectRatio: true
+      }}/>
   }
 }
--- src/containers/Graph/tooltips.js
+++ src/containers/Graph/tooltips.js
@@ -19,3 +19,25 @@ export function graphTooltip (tooltipModel) {
     tooltip.style.opacity = 1
   })
 }
+
+export function doughnutTooltip (tooltipModel) {
+  const tooltip = document.getElementById('tooltip-' + this._chart.id)
+
+  if (!tooltipModel || !tooltipModel.opacity) return
+  if (!tooltipModel.dataPoints.length) return
+
+  console.log(this, tooltipModel)
+
+  tooltipModel.dataPoints.forEach(datapoint => {
+    const [ label, value ] = tooltipModel.body[0].lines[0].split(':')
+    const content = `<span class="label">` + label + `</span>
+                     <span class="value">` + value + `</span>`
+    tooltip.innerHTML = content
+
+    const posX = _.clamp(datapoint.x, 60, this._chart.canvas.offsetWidth - 60)
+
+    tooltip.style.left = posX + 'px'
+    tooltip.style.top = (this._chart.canvas.offsetHeight / 2 + 4) + 'px'
+    tooltip.style.opacity = 1
+  })
+}
